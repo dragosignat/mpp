8c65cab99ad1134d6e570f2f3f29bb83d7ee8d7a
Dragos Ignat <dragos@Dragoss-MBP.cs.ubbcluj.ro>
2024-05-21 12:37:30 +0300

    small fix

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'http://localhost:8080/apiv1';
+// Get the API URL from the environment variable
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/apiv1';
