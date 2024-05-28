dc2013b44a155b213bd713e5ab172bac352eccd0
Dragos Ignat <dragos@Dragoss-MBP.cs.ubbcluj.ro>
2024-05-28 12:22:46 +0300

    Added Self signed SSL certificate (todo: nginx settings)

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,4 +1,3 @@
 // Get the API URL from the environment variable
-// export const API_URL =
-// process.env.REACT_APP_API_URL || 'http://localhost:8080/apiv1';
-export const API_URL = 'http://localhost:8080/apiv1';
+export const API_URL =
+    import.meta.env.VITE_API_URL || 'http://localhost:8080/apiv1';
